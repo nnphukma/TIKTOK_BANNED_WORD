3d57538661d24c0ddb86b25692098ddbc5c6302a
Nguyễn Ngọc Phú <157860410+nnphukma@users.noreply.github.com>
2024-03-26 08:08:39 +0700

    update found bannedword

--- Check_Banned_word.cpp
+++ Check_Banned_word.cpp
@@ -1,4 +1,3 @@
-//link tải DEVC++: https://www.embarcadero.com/free-tools/dev-cpp //
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -11,28 +10,45 @@ string toLowerCase(string str) {
     return str;
 }
 
-string checkWords(string sentence) {
-    vector<string> keywords = {"hello kitty", "zach bryan", "zach" ,"bryan", "get in loser", "kid", "hallmark", "charlie", "snoopy", "comfort colors", "embroidered", "disney", "mickey", "svg", "png", "fuck", "cut", "baby", "yup", "nutcracker", "funny quote i will put you in a trunk women premium t-shirt", "labor and delivery", "nurse christmas shirt", "special deliveries shirt", "labor tech shirts", "l&d santa", "labor and delivery holiday shirts", "r2-d2", "i will put you in a trunk and help people look for you premium t-shirt", "flower shirt gift for her wild flower shirt aesthetic floral graphic tee wildflower shirt flower t-shirt botanical shirt wildflower tshirt stylish and warm t-shirts", "crewnecks", "hoodies for all", "nurse ghost i will stab you t-shirt funny halloween gift", "barbenheimer", "taylor swift","taylor", "jonas brothers", "winnie the pooh", "i am kenough", "spiderman", "jonas", "grinch", "l&d", "labor tech", "zach bryan", "get in loser", "kid", "hallmark", "charlie", "snoopy", "barbenheimer", "taylor swift", "jonas brothers","jonas", "winnie the pooh", "i am kenough", "spiderman", "jonas", "grinch", "l&d", "labor tech", "zach bryan", "get in loser", "kid", "hallmark", "charlie", "snoopy", "comfort colors®", "embroidered", "disney", "mickey", "svg", "png", "fuck", "cut", "baby", "yup", "nutcracker", "travis scott", "barbie", "swiftie", "bts", "politics", "game (zelda)", "kenny west", "ducks the sopranos", "nude", "kid", "child", "children", "toddler", "barbie", "phyladel phia", "the weeknd", "carhartt cow", "olivia rodrigo", "jordan", "active shooter", "palestine", "retro folklore", "hello kitty"};
-
-    sentence = toLowerCase(sentence);
+vector<string> checkWords(string sentence) {
+vector<string> keywords = {"zach bryan", "zach", "bryan", "get in loser", "kid", "hallmark", "charlie", "snoopy", "comfort colors", "embroidered", "disney", "mickey", "svg", "png", "fuck", "cut", "baby", "yup", "nutcracker", "funny quote i will put you in a trunk women premium t-shirt", "labor and delivery nurse christmas shirt, special deliveries shirt, labor tech shirts", "l&d santa shirt", "labor and delivery holiday shirts", "r2-d2", "i will put you in a trunk and help people look for you premium t-shirt", "flower shirt gift for her wild flower shirt aesthetic floral graphic tee wildflower shirt flower t-shirt botanical shirt wildflower tshirt stylish and warm t-shirts", "crewnecks", "hoodies for all", "nurse ghost i will stab you t-shirt funny halloween gift", "barbenheimer", "taylor swift","taylor", "jonas brothers", "jonas", "winnie the pooh", "i am kenough","kenough", "spiderman", "grinch", "l&d", "labor tech", "zach bryan", "get in loser", "kid", "hallmark", "charlie", "snoopy", "comfort colors", "embroidered", "disney", "mickey", "svg", "png", "fuck", "cut", "baby", "yup", "nutcracker", "travis scott", "travis", "barbie", "swiftie", "bts", "politics", "game (zelda)", "zelda", "kenny west", "kenny", "west", "ducks the sopranos", "sopranos", "kid", "child", "children", "toddler", "barbie", "philadelphia", "the weeknd", "carhartt cow", "olivia rodrigo", "olivia", "rodrigo", "jordan", "active shooter", "palestine", "retro folklore", "retro", "folklore", "hello kitty", "hoodie for all"};
+   sentence = toLowerCase(sentence);
+    vector<string> foundKeywords;
     for (const string& keyword : keywords) {
         if (sentence.find(keyword) != string::npos) {
-            return "BANNED";
+            foundKeywords.push_back(keyword);
         }
     }
 
-    return "NOT BANNED";
+    return foundKeywords;
+
 }
 
 int main() {
-	int n = 20;
-	while(n--){
-	for(int i = 0; i< n; i++){
-		string input;
-	    cout << "CheckKeyWord: ";
-	    getline(cin, input);
-	    cout << "Return: " << checkWords(input) << endl;
-	}
-	}
-return 0;
+	vector<string> testCases;
+    string input;
+    int n = 20;
+
+    while (n--) {
+        cout << "TestCase #" << (20 - n) << ": ";
+        getline(cin, input);
+        testCases.push_back(input);
+
+        vector<string> foundKeywords = checkWords(input);
+        if (!foundKeywords.empty()) {
+            cout << "Loading KeyBanned: ";
+            for (const string& keyword : foundKeywords) {
+                cout << " " << keyword << ",";
+            }
+            cout << endl;
+        }
+       else {
+       	cout << "NOT BANNED" << endl;
+	   }
+        cout << endl;
+    }
+
+    cout << "Kết thúc test cases" << endl;
+
+    return 0;
 }
